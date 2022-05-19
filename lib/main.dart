3676bae04be0e72456f1338b9499ac2a1d756724
Nuri <nuriddin7612@mail.ru>
2022-05-20 01:40:40 +0900

    Practice_App

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MyApp()
-  );
+  runApp(const MyApp());
 }
+
+int numberOfPresses = 0;
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -14,9 +14,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'RandomApp',
       home: Scaffold(
-
+        appBar: AppBar(
+          title: const Center(child: Text('Random App')),
+        ),
+        body: const PressCount(),
       ),
     );
   }
 }
+
+class PressCount extends StatefulWidget {
+  const PressCount({Key? key}) : super(key: key);
+
+  @override
+  State<PressCount> createState() => _PressCountState();
+}
+
+class _PressCountState extends State<PressCount> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'You have pressed the screen',
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            '$numberOfPresses',
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 8),
+          const Text(
+            'many times',
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 
